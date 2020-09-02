53047778a62148da84e43873986dfa42880cf81a
Rytis <rytis.augustauskas1@gmail.com>
2020-09-02 13:38:34 +0300

    a few changes for debugging

--- Application/main.cpp
+++ Application/main.cpp
@@ -4,15 +4,23 @@
 #include "opencv2/imgproc.hpp"
 #include "opencv2/imgcodecs.hpp"
 #include "opencv2/videoio.hpp"
+#include <iostream>
 
 int main()
 {
 	SegmentationNeuralNetwork network;
 	std::string modelPath = "C:/src/RoadPavementSegmentation/models/frozen_models/UNet4_res_assp_5x5_24k_128x128_e09_leaky_as_relu.pb";
-	modelPath = "C:/src/RoadPavementSegmentation/models/frozen_models/UNet4_res_assp_5x5_16k_320x320_e14.pb";
+	modelPath = "C:/src/Projects/OpenCVDNNTest/models/UNet4_res_assp_5x5_24k_128x128_e09_leaky_relu.pb";
 	network.Init(modelPath);
-	std::string imagePath = "C:/Users/prorega/Desktop/fredaTest/hole.jpg";
+	std::string imagePath = "C:/src/Projects/OpenCVDNNTest/images/hole.jpg";
 	cv::Mat image = cv::imread(imagePath, cv::ImreadModes::IMREAD_GRAYSCALE);
-	cv::Mat prediction = network.Predict(image);
-	int h = 4;
+	try
+	{
+		cv::Mat prediction = network.Predict(image);
+	}
+	catch (cv::Exception& ex)
+	{
+		std::cout << ex.what() << std::endl;
+	}
+	return 0;
 }
